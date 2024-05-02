3651d48a20a1535616453f0367158fa368571853
mike <mikefc@coolbutuseless.com>
2024-05-02 22:47:19 +1000

    svg refactor

--- R/core-html.R
+++ R/core-html.R
@@ -168,24 +168,26 @@ as_svg <- function(x, width = 1200, height = 900, ...) {
 #'
 #' @inheritParams as_html
 #' @param width,height viewBox dimensions for SVG
+#' @param extra extra tags to insert into group. default NULL
 #'
 #' @return character string containing an SVG snippet.
 #'
 #' @export
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-as_svg_group <- function(x, width = 1200, height = 900, ...) {
+as_svg_group <- function(x, width = 1200, height = 900, visible = TRUE, extra = NULL, ...) {
 
   res <- as_html(x, ...)
 
 
   svg_text <- paste(
     "",
-    "<g>",
+    ifelse(visible, '<g visibility="visible">', '<g visibility="hidden">'),
     '<foreignObject width=\"100%\" height=\"100%\">',
     '<div xmlns=\"http://www.w3.org/1999/xhtml\">',
     res,
     "</div>",
     "</foreignObject>",
+    extra,
     "</g>",
     "",
     sep = "\n"
@@ -198,6 +200,79 @@ as_svg_group <- function(x, width = 1200, height = 900, ...) {
 
   svg_text
 }
+
+
+make_animate_tag <- function(i, n) {
+  if (i == 1) {
+    sprintf('<animate id="img%03i" attributeName="visibility" begin="0s; img%03i.end"
+            values="hidden;visible;visible;" dur="2s" fill="freeze" />', i, n)
+  } else {
+    sprintf('<animate id="img%03i" attributeName="visibility" begin="img%03i.end"
+            values="hidden;visible;visible;" dur="2s" fill="freeze"/>', i, i - 1)
+  }
+}
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' Wrap an emphatic object into an SVG for display in a github README.md
+#'
+#' Idea borrowed from pointblank
+#'
+#' @inheritParams as_html
+#' @param width,height viewBox dimensions for SVG
+#' @param ... multiple emphatic objects
+#'
+#' @return character string containing an SVG snippet.
+#'
+#' @export
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+as_svg_anim <- function(..., width = 1200, height = 900) {
+
+  # <animate attributeName="visibility" begin="svg1.click" dur="3s" from="visible" to="hidden" repeatCount="indefinite" />
+
+  objs <- list(...)
+  groups <- vector('list', length(objs))
+
+  for (i in seq_along(objs)) {
+    groups[[i]] <- as_svg_group(objs[[i]], width = width, height = height,
+                                visible = FALSE,
+                                extra = make_animate_tag(i, length(objs)))
+  }
+
+  svg_text <- paste(
+    "<svg id=\"emphatic-anim\" fill=\"none\" viewBox=\"0 0 ", width, " ", height,
+    "\" width=\"", width, "\" height=\"", height, "\" xmlns=\"http://www.w3.org/2000/svg\">",
+    paste(unlist(groups), collapse = ""),
+    # groups[[1]],
+    # as_svg_group(objs[i], width = width, height = height, extra = NULL),
+    '</svg>',
+    collapse = "\n"
+  )
+
+
+  class(svg_text) <- unique(c('knit_asis', class(svg_text)))
+
+  svg_text
+}
+
+
+if (FALSE) {
+
+  as_svg_anim(hl_grep(mode, "switch"), hl_diff("hello", "there")) |> cat()
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
